c19a248e5d330ae4e1103f75df586eaab7856ddb
Igor <igor.stanevoj@gmail.com>
2018-04-04 20:29:38 +0300

    Add files via upload

---
+++ D3js/js/script.js
@@ -0,0 +1,52 @@
+//Width and height
+var w = 1200;
+var h = 200;
+var barPadding = 1;
+			
+var dataset = [ 4, 8, 16, 32, 46, 42, 36, 27, 20, 40,
+							30, 27, 49, 39, 30, 27, 38, 10, 17, 29, 34, 14, 24, 40 ];
+			
+d3.select("body")
+	.append("h1")
+	.text("Пример столбиковой диаграммы D3 js");
+
+var svg = d3.select("body")
+						.append("svg")
+						.attr("width", w)
+						.attr("height", h);
+
+			svg.selectAll("rect")
+			   .data(dataset)
+			   .enter()
+			   .append("rect")
+			   .attr("x", function(d, i) {
+			   		return i * (w / dataset.length);
+			   })
+			   .attr("y", function(d) {
+			   		return h - (d * 4);
+			   })
+			   .attr("width", w / dataset.length - barPadding)
+			   .attr("height", function(d) {
+			   		return d * 4;
+			   })
+			   .attr("fill", function(d) {
+					return "rgb(0, 0, " + (d * 10) + ")";
+			   });
+
+			svg.selectAll("text")
+			   .data(dataset)
+			   .enter()
+			   .append("text")
+			   .text(function(d) {
+			   		return d;
+			   })
+			   .attr("text-anchor", "middle")
+			   .attr("x", function(d, i) {
+			   		return i * (w / dataset.length) + (w / dataset.length - barPadding) / 2;
+			   })
+			   .attr("y", function(d) {
+			   		return h - (d * 4) + 14;
+			   })
+			   .attr("font-family", "sans-serif")
+			   .attr("font-size", "15px")
+			   .attr("fill", "white");
